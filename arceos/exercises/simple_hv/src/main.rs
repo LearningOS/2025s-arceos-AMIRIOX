ac72ac83218dc1a8a5d5d18d86fcf21fc13ad35a
AMIRIOX <wumingyun2120@outlook.com>
2025-04-18 10:48:08 +0800

    [600/600] support hypervisor

--- arceos/exercises/simple_hv/src/main.rs
+++ arceos/exercises/simple_hv/src/main.rs
@@ -77,7 +77,7 @@ fn run_guest(ctx: &mut VmCpuRegisters) -> bool {
 
 #[allow(unreachable_code)]
 fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
-    use scause::{Exception, Trap};
+    use scause::{Exception, Interrupt, Trap};
 
     let scause = scause::read();
     match scause.cause() {
@@ -102,16 +102,27 @@ fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
             }
         },
         Trap::Exception(Exception::IllegalInstruction) => {
+            ctx.guest_regs.gprs.set_reg(regs::GprIndex::A0, 0x6688);
+            ctx.guest_regs.sepc += 4;
+            /*
             panic!("Bad instruction: {:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
+            */
         },
         Trap::Exception(Exception::LoadGuestPageFault) => {
+            ctx.guest_regs.gprs.set_reg(regs::GprIndex::A1, 0x1234);
+            ctx.guest_regs.sepc += 4;
+            /*
             panic!("LoadGuestPageFault: stval{:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
+            */
+        },
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            ctx.guest_regs.sepc += 4;
         },
         _ => {
             panic!(
